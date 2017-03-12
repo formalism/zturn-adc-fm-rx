44dc8a6b5aaaa6cb6618055cc59622a444ac5830
Tomoyuki Ueda <tu@formalism.net>
2017-03-12 23:32:10 +0900

    Add PC software

---
+++ PC/adc-capture/adc-capture/FormMain.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace adc_capture
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_capture = new System.Windows.Forms.Button();
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_capture
+            // 
+            this.button_capture.Location = new System.Drawing.Point(12, 65);
+            this.button_capture.Name = "button_capture";
+            this.button_capture.Size = new System.Drawing.Size(118, 63);
+            this.button_capture.TabIndex = 0;
+            this.button_capture.Text = "capture";
+            this.button_capture.UseVisualStyleBackColor = true;
+            this.button_capture.Click += new System.EventHandler(this.button_capture_Click);
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Location = new System.Drawing.Point(12, 37);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Size = new System.Drawing.Size(118, 22);
+            this.textBox_IP.TabIndex = 1;
+            this.textBox_IP.Text = "192.168.0.90";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.textBox_IP);
+            this.Controls.Add(this.button_capture);
+            this.Name = "FormMain";
+            this.Text = "Capture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_capture;
+        private System.Windows.Forms.TextBox textBox_IP;
+    }
+}
+
---
+++ PC/adc-capture/adc-capture/FormMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adc_capture
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        public static int SERVER_PORT = 12345;
+        public static string SERVER_ADR = "192.168.56.101";
+
+        private void button_capture_Click(object sender, EventArgs e)
+        {
+            TcpClient tcp;
+            string ip = textBox_IP.Text;
+            if (ip != null)
+                tcp = new TcpClient(ip, SERVER_PORT);
+            else
+                tcp = new TcpClient(SERVER_ADR, SERVER_PORT);
+
+            byte[] buf = new byte[1024*1024];
+            using (NetworkStream ns = tcp.GetStream())
+            {
+                uint i = 0;
+                Stopwatch sw = Stopwatch.StartNew();
+                while (true)
+                {
+                    int total = 0;
+                    while (total < buf.Length)
+                    {
+                        int len = ns.Read(buf, 0, buf.Length-total);  // Read 1MBytes
+                        total += len;
+                    }
+                    i++;
+                    if (i >= 256)    // 256MBytes
+                        break;
+                }
+                sw.Stop();
+                MessageBox.Show(sw.ElapsedMilliseconds + "[ms]");
+            }
+        }
+    }
+}
